0eacd47e850c172dfa55368ee7663aa9d36c15f3
Vladimir Pasquier <vpasquier@nuxeo.com>
2018-11-16 10:49:12 -0500

    NXP-26088: add zapier widget manipulation example

--- nuxeo-zapier-app/triggers/project.js
+++ nuxeo-zapier-app/triggers/project.js
@@ -3,9 +3,17 @@ const triggerProject = (z, bundle) => {
     url: 'http://zapier.apps.prod.nuxeo.io/nuxeo/api/v1/search/pp/default_document_suggestion/execute',
     params: {},
   };
-
   return z.request(request).then((response) => {
     const projects = JSON.parse(response.content).entries;
+    // Test for manipulating zapier widgets
+    const input = {
+      key: 'example',
+      type: 'string',
+      required: true,
+      label: 'example',
+    };
+    z.console.log(JSON.stringify(bundle));
+    operation.inputFields.push(input);
     return projects.map((project) => {
       project.id = project.uid;
       delete project.uid;

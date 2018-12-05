8a9af95616d71591c584c0d3e29c37044b729e59
Vladimir Pasquier <vpasquier@nuxeo.com>
2018-12-05 17:24:13 -0500

    NXP-26390: add new zapier notifier

---
+++ nuxeo-zapier-webhook/src/main/java/org/nuxeo/zapier/webhook/WebHookNotifier.java
@@ -0,0 +1,45 @@
+/*
+ * (C) Copyright 2018 Nuxeo (http://nuxeo.com/) and others.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Contributors:
+ *      Nuxeo
+ */
+
+package org.nuxeo.zapier.webhook;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.nuxeo.ecm.notification.message.Notification;
+import org.nuxeo.ecm.notification.notifier.Notifier;
+import org.nuxeo.ecm.notification.notifier.NotifierDescriptor;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.zapier.ZapierComponent;
+
+/**
+ * @since 0.1
+ */
+public class WebHookNotifier extends Notifier {
+    private static final Logger log = LogManager.getLogger(WebHookNotifier.class);
+
+    public WebHookNotifier(NotifierDescriptor desc) {
+        super(desc);
+    }
+
+    @Override
+    public void process(Notification notification) {
+        ZapierComponent zapierComponent = Framework.getService(ZapierComponent.class);
+        // zapierComponent.sendEventBundle();
+    }
+
+}

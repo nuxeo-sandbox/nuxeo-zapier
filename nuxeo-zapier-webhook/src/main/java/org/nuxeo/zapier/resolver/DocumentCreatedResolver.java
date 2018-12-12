ac4d839a8b4714945eb6f9233de7d2cb5c3babb3
Arnaud Kervern <arnaud.k@gmail.com>
2018-12-12 15:20:38 -0500

    NXP-26456: Add document created resolver

---
+++ nuxeo-zapier-webhook/src/main/java/org/nuxeo/zapier/resolver/DocumentCreatedResolver.java
@@ -0,0 +1,48 @@
+/*
+ * (C) Copyright 2018 Nuxeo (http://nuxeo.com/) and others.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Contributors:
+ *      Nuxeo
+ */
+
+package org.nuxeo.zapier.resolver;
+
+import static org.nuxeo.ecm.core.api.event.DocumentEventTypes.DOCUMENT_CREATED;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.nuxeo.ecm.notification.message.EventRecord;
+import org.nuxeo.ecm.notification.resolver.SubscribableResolver;
+
+public class DocumentCreatedResolver extends SubscribableResolver {
+    protected static final String DOCUMENT_TYPE = "documentType";
+
+    @Override
+    public List<String> getRequiredContextFields() {
+        return Arrays.asList();
+    }
+
+    @Override
+    public boolean accept(EventRecord eventRecord) {
+        return DOCUMENT_CREATED.equals(eventRecord.getEventName());
+    }
+
+    @Override
+    public Map<String, String> buildNotifierContext(EventRecord eventRecord) {
+        return Collections.singletonMap(DOCUMENT_TYPE, eventRecord.getDocumentSourceType());
+    }
+}
